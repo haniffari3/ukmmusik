8153f01a22497e4e36dd5ce91e38ac9289f1a850
haniffari3 <farhanif@gmail.com>
2025-10-08 08:39:07 +0700

    first posting web app from github

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "27deb067143c762903928d8ea452afdc
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "54737c82fecbf570514f5b121a6ba0da",
+"flutter_bootstrap.js": "d18d983556eca19d5f8b38297af49709",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
