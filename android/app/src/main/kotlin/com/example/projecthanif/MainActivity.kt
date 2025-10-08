d2ceebef2effd43c3d44d0e93ee6b43f699c7929
haniffari3 <farhanif@gmail.com>
2025-10-08 08:30:17 +0700

    first commit

--- android/app/src/main/kotlin/com/example/projecthanif/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.projecthanif
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/constants/theme.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-final ThemeData appTheme = ThemeData(
-  primarySwatch: Colors.deepPurple,
-  textTheme: GoogleFonts.poppinsTextTheme(),
-);
